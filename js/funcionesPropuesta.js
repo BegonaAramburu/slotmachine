4f96aec696add2cb16d013741887e1bbe7636163
BegonaAramburu <begona.aramburu@gmail.com>
2019-06-30 08:16:32 +0200

    cambio del =10

--- js/funcionesPropuesta.js
+++ js/funcionesPropuesta.js
@@ -53,61 +53,64 @@
  * @returns
  */
 	
-function mostrarPremio(){
-	if(premio!=0){
-		document.getElementById("alertaPremio").innerHTML = premio + " PUNTOS";
+	function alertaPremio(premio){
+		if(premio!=0){
+			document.getElementById("alertaPremio").innerHTML = premio + " PUNTOS";
+		}
 	}
-}	
 	
 	
 	
 //1.Definir el evento onclick del botón id="inicio"
 
-var puntos = 100;
-var premio;
-document.getElementById("inicio").onclick = function(){
-		
-	document.getElementById("puntos").value = puntos;
-}
+	var puntos = 100;
+	var premio;
+	document.getElementById("inicio").onclick = function(){
 			
-//2.Definir el evento onclick del botón id="tirar"	
-	
-document.getElementById("tirar").onclick = function(){
-	puntos = puntos-10;//en cada tirada restará 10 puntos de la apuesta
-	premio = 0;
-	//crear una variable array que contiene todas las imágenes
-	var imagenes = ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg", "img/img4.jpg", "img/img5.jpg", "img/img6.jpg"];
-	console.log(imagenes);
-	//3.Llamamos a la funcion randomArray que nos dará 3 posiciones del Array al azar y las guardamos en las variables bomboA B y C
-	
-	var bomboA = randomArray(imagenes);
-	var bomboB = randomArray(imagenes);
-	var bomboC = randomArray(imagenes);
-	
-	//Las guardamos en su correspondiente elemento HTML con id "bomboA" B y C
-	document.getElementById("bomboA").src = imagenes[bomboA];
-	document.getElementById("bomboB").src = imagenes[bomboB];
-	document.getElementById("bomboC").src = imagenes[bomboC];
-	
-	console.log(bomboA);
-	console.log(bomboB);
-	console.log(bomboC);
-	
-	//4.Llamamos a la función darPremio
-	
-	//computar los puntos dependiendo del premio obtenido			
-	puntos = puntos + darPremio(bomboA, bomboB, bomboC);
-	
-	//escribir los puntos obtenidos en el input de id="puntos"
-	document.getElementById("puntos").value = puntos;
-	alert(puntos);//por qué no sale?????????
-	//5. Si hay premio sale una alerta de premio
-	
-	//mostrarPremio();
-	
-			
-	//Condición para dejar de jugar
-	if(puntos<=10){
-	document.getElementById("puntos").value = "No te quedan puntos";
+		document.getElementById("puntos").value = puntos;
 	}
-}
+				
+	//2.Definir el evento onclick del botón id="tirar"	
+		
+	document.getElementById("tirar").onclick = function(){
+		document.getElementById("alertaPremio").innerHTML ="";
+		
+		puntos = puntos-10;//en cada tirada restará 10 puntos de la apuesta
+		premio = 0;
+		//crear una variable array que contiene todas las imágenes
+		var imagenes = ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg", "img/img4.jpg", "img/img5.jpg", "img/img6.jpg"];
+		console.log(imagenes);
+		//3.Llamamos a la funcion randomArray que nos dará 3 posiciones del Array al azar y las guardamos en las variables bomboA B y C
+		
+		var bomboA = randomArray(imagenes);
+		var bomboB = randomArray(imagenes);
+		var bomboC = randomArray(imagenes);
+		
+		//Las guardamos en su correspondiente elemento HTML con id "bomboA" B y C
+		document.getElementById("bomboA").src = imagenes[bomboA];
+		document.getElementById("bomboB").src = imagenes[bomboB];
+		document.getElementById("bomboC").src = imagenes[bomboC];
+		
+		console.log(bomboA);
+		console.log(bomboB);
+		console.log(bomboC);
+		
+		//4.Llamamos a la función darPremio
+		
+		//computar los puntos dependiendo del premio obtenido			
+		puntos = puntos + darPremio(bomboA, bomboB, bomboC);
+		
+		alertaPremio(premio);
+		
+		//escribir los puntos obtenidos en el input de id="puntos"
+		document.getElementById("puntos").value = puntos;	
+				
+		//Condición para dejar de jugar
+		if(puntos<10){
+		document.getElementById("puntos").value = "No te quedan puntos";
+		}
+	}
+
+	
+	
+	
